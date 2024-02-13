4950702d6257172c5fa425304958f69370c5b94e
livupato <alanprofeta40@gmail.com>
2024-02-13 01:42:57 -0300

    estrutura base do bot
    ainda tentando arrumar o config,json (ignorado pelo git)

---
+++ app.py
@@ -0,0 +1,22 @@
+import discord
+import json
+token = json.load(config.json)
+print(token)
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('/kelvin'):
+        await message.channel.send('macaco')
+
+client.run('')
